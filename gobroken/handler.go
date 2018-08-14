f4807d9c1a35546d77c2b9829770830af5d6e4e0
Martin Dekov (VMware) <mdekov@vmware.com>
2018-08-14 08:33:49 -0700

    working!

--- gobroken/handler.go
+++ gobroken/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", wat?)
+	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
 }
